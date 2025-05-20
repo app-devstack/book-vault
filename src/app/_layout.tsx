39de930731f578464b90c9a4be96df041fbf1a84
rikiTada <riki.tada777@gmail.com>
2025-05-21 00:56:45 +0900

    🚧 WIP: 初回マイグレーションのお試し実装

--- src/app/_layout.tsx
+++ src/app/_layout.tsx
@@ -1,13 +1,14 @@
+import RootLayoutProvider from "@/components/providers/root-layout-provider";
 import { Stack } from "expo-router";
 import { StatusBar } from "react-native";
 
 export default function RootLayout() {
   return (
-    <>
+    <RootLayoutProvider>
       <StatusBar barStyle="light-content" backgroundColor={"black"} />
       <Stack>
         <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
       </Stack>
-    </>
+    </RootLayoutProvider>
   );
 }
