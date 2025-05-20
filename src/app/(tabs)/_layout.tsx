216544ab5ee62704b72aa04112ff39779ef839cc
rikiTada <riki.tada777@gmail.com>
2025-05-21 00:07:33 +0900

    🚚 ファイルの大移動その2

--- src/app/(tabs)/_layout.tsx
+++ src/app/(tabs)/_layout.tsx
@@ -27,7 +27,7 @@ export default function TabLayout() {
       }}
     >
       <Tabs.Screen
-        name="home"
+        name="index"
         options={{
           // headerLargeTitle: true,
           headerTitle: appName || "book store",
