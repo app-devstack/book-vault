1402ec02f420869c6bd02a2e105a92e5191e16fc
tada rikimaru <122006815+rikiTada@users.noreply.github.com>
2025-05-28 13:02:44 +0900

    🐣 検索クエリの更新

--- src/feature/register/search-books.tsx
+++ src/feature/register/search-books.tsx
@@ -31,7 +31,7 @@ import {
 
 const fetchBooks = async (query: string) => {
   const response = await fetch(
-    `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(query)}`
+    `https://www.googleapis.com/books/v1/volumes?q=intitle:${encodeURIComponent(query)}&langRestrict=ja`
   );
   if (!response.ok) {
     throw new Error("データの取得に失敗しました");
