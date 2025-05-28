c4b27f37b43a4680640dfff6141a1578e89717ac
rikiTada <riki.tada777@gmail.com>
2025-05-29 00:20:05 +0900

    🍨 safe areaを設置し、フラットリストの表示領域を指定

--- src/components/providers/root-layout-provider.tsx
+++ src/components/providers/root-layout-provider.tsx
@@ -1,6 +1,7 @@
 import DBProvider from "@/components/providers/db-provider";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { ReactNode } from "react";
+import { SafeAreaProvider, SafeAreaView } from "react-native-safe-area-context";
 import Toast from "react-native-toast-message";
 
 const queryClient = new QueryClient();
@@ -11,9 +12,13 @@ export default function RootLayoutProvider({
   children: ReactNode;
 }) {
   return (
-    <QueryClientProvider client={queryClient}>
-      <DBProvider>{children}</DBProvider>
-      <Toast />
-    </QueryClientProvider>
+    <SafeAreaProvider>
+      <SafeAreaView style={{ flex: 1 }} edges={["left", "right"]}>
+        <QueryClientProvider client={queryClient}>
+          <DBProvider>{children}</DBProvider>
+          <Toast />
+        </QueryClientProvider>
+      </SafeAreaView>
+    </SafeAreaProvider>
   );
 }
