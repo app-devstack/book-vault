b96221613f0f2c2db8b635f0ec079680a012f174
rikiTada <riki.tada777@gmail.com>
2025-06-05 22:36:43 +0900

    ✨ Metro.config.jsを更新し、CSSサポートを有効化

--- metro.config.js
+++ metro.config.js
@@ -2,9 +2,9 @@
 const { getDefaultConfig } = require("expo/metro-config");
 
 /** @type {import('expo/metro-config').MetroConfig} */
-const config = getDefaultConfig(__dirname);
-
-// drizzle sqlite settings
-config.resolver.sourceExts.push("sql");
+const config = getDefaultConfig(__dirname, {
+  // [Web-only]: Enables CSS support in Metro.
+  isCSSEnabled: true,
+});
 
 module.exports = config;
