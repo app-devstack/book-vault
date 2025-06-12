38dd98eea94930644a7b0504c5bbe57a8fe32f26
rikiTada <riki.tada777@gmail.com>
2025-06-12 22:52:52 +0900

    🩹 スタッシュファイルの型エラーをコメントアウト

--- src/features/_stashes/register/components/book-search/index.tsx
+++ src/features/_stashes/register/components/book-search/index.tsx
@@ -1,27 +1,27 @@
-import { Text } from "@/components/Text";
-import { useDebounce } from "@uidotdev/usehooks";
-import React, { useState } from "react";
-import { SearchForm } from "../SearchForm";
-import { SearchResults } from "../SearchResults";
-import { useGetBookSearch } from "../hooks/useBookSearch";
+// import { Text } from "@/components/Text";
+// import { useDebounce } from "@uidotdev/usehooks";
+// import React, { useState } from "react";
+// import { SearchForm } from "../SearchForm";
+// import { SearchResults } from "../SearchResults";
+// import { useGetBookSearch } from "../hooks/useBookSearch";
 
-//   Debounce
-const SEARCH_INPUT_DEBOUNCE_DELAY = 500; // ミリ秒
+// //   Debounce
+// const SEARCH_INPUT_DEBOUNCE_DELAY = 500; // ミリ秒
 
-export default function BookSearch() {
-  const delay = SEARCH_INPUT_DEBOUNCE_DELAY;
-  const [searchTerm, setSearchTerm] = useState("");
-  const debouncedTerm = useDebounce(searchTerm, delay);
+// export default function BookSearch() {
+//   const delay = SEARCH_INPUT_DEBOUNCE_DELAY;
+//   const [searchTerm, setSearchTerm] = useState("");
+//   const debouncedTerm = useDebounce(searchTerm, delay);
 
-  const { data, isLoading } = useGetBookSearch(debouncedTerm);
+//   const { data, isLoading } = useGetBookSearch(debouncedTerm);
 
-  return (
-    <>
-      <SearchForm searchTerm={searchTerm} onSearchTermChange={setSearchTerm} />
+//   return (
+//     <>
+//       <SearchForm searchTerm={searchTerm} onSearchTermChange={setSearchTerm} />
 
-      {isLoading && <Text>読み込み中...</Text>}
+//       {isLoading && <Text>読み込み中...</Text>}
 
-      {data && <SearchResults data={data} />}
-    </>
-  );
-}
+//       {data && <SearchResults data={data} />}
+//     </>
+//   );
+// }
--- src/features/_stashes/register/register-contents.tsx
+++ src/features/_stashes/register/register-contents.tsx
@@ -1,17 +1,17 @@
-import BookSearch from "@/features/_stashes/register/components/book-search";
-import { StyleSheet, View } from "react-native";
+// import BookSearch from "@/features/_stashes/register/components/book-search";
+// import { StyleSheet, View } from "react-native";
 
-export default function RegisterContents() {
-  return (
-    <View style={styles.container}>
-      <BookSearch />
-    </View>
-  );
-}
+// export default function RegisterContents() {
+//   return (
+//     <View style={styles.container}>
+//       <BookSearch />
+//     </View>
+//   );
+// }
 
-const styles = StyleSheet.create({
-  container: {
-    padding: 16,
-    gap: 16,
-  },
-});
+// const styles = StyleSheet.create({
+//   container: {
+//     padding: 16,
+//     gap: 16,
+//   },
+// });
