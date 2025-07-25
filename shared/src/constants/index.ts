3cabbf0bc810d5fd24b523593d76e90c82eca308
rikiTada <riki.tada777@gmail.com>
2025-07-26 01:53:10 +0900

    🚚 monorepo変更。共通のパッケージ作成

---
+++ shared/src/constants/index.ts
@@ -0,0 +1,13 @@
+/** シリーズ未指定のときのデフォルトID */
+export const EMPTY_SERIES_ID = '01976464-7d02-7f01-9fa1-2d6ac90450a8';
+
+/** ショップ未指定のときのデフォルトID */
+export const EMPTY_SHOP_ID = '01976488-9d25-73fe-95e9-68d05e783406';
+
+/**
+ * APIキー
+ * @todo そのうち、envで管理するので一旦仮の値を設定している
+ */
+export const API_ACCESS_KEY = 'bv-server-01982576-e4b3-7457-919d-abb0aee6ccb6';
+
+export const SERVER_URL = 'https://bv-server.maru-maru.workers.dev/api/backup';
