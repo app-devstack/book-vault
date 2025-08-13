ef4d48ed3e3e9633645bb4597acd86f954631a85
rikiTada <riki.tada777@gmail.com>
2025-08-13 19:12:03 +0900

    ♻️ GestureHandlerRootViewを追加して、EditableSeriesListをラップ

--- src/features/home/components/SeriesEditMode.tsx
+++ src/features/home/components/SeriesEditMode.tsx
@@ -3,6 +3,8 @@ import React from 'react';
 import { EditModeHeader } from '@/features/home/components/editable/EditModeHeader';
 import { EditableSeriesList } from '@/features/home/components/editable/EditableSeriesList';
 import { SeriesWithBooks } from '@/features/home/types';
+import { GestureHandlerRootView } from 'react-native-gesture-handler';
+
 import { SeriesStats } from '@/hooks/screens/useHomeScreen';
 
 interface SeriesEditModeProps {
@@ -23,13 +25,9 @@ export const SeriesEditMode: React.FC<SeriesEditModeProps> = ({
   isUpdating,
 }) => {
   return (
-    <>
+    <GestureHandlerRootView style={{ flex: 1 }}>
       <EditModeHeader onCancel={onCancel} onSave={onSave} isUpdating={isUpdating} />
-      <EditableSeriesList
-        data={data}
-        onDragEnd={onDragEnd}
-        getSeriesStats={getSeriesStats}
-      />
-    </>
+      <EditableSeriesList data={data} onDragEnd={onDragEnd} getSeriesStats={getSeriesStats} />
+    </GestureHandlerRootView>
   );
 };
